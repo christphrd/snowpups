a5b82bd1d058436fe2be0e311ab78de613cb7eab
github username <github email address>
2018-04-10 15:27:38 -0400

    updated controllers for post, patch and delete

--- app/controllers/api/v1/dogs_controller.rb
+++ app/controllers/api/v1/dogs_controller.rb
@@ -8,4 +8,31 @@ class Api::V1::DogsController < ApplicationController
     @dog = Dog.find(params[:id])
     render json: @dog
   end
+
+  def create
+    @dog = Dog.new(dog_params)
+    if @dog.valid?
+      @dog.save
+      render json: @dog
+    end
+  end
+
+  def update
+    @dog = Dog.find(params[:id])
+    if @dog.update(dog_params)
+      render json: @dog
+    end
+  end
+
+  def destroy
+    @dog = Dog.find(params[:id])
+    @dog.destroy
+  end
+
+
+  private
+
+  def dog_params
+    params.require(:dog).permit(:name, :breed, :imageUrl, :sledId, :isGoodDog)
+  end
 end
--- app/controllers/api/v1/sleds_controller.rb
+++ app/controllers/api/v1/sleds_controller.rb
@@ -8,4 +8,31 @@ class Api::V1::SledsController < ApplicationController
     @sled = Sled.find(params[:id])
     render json: @sled
   end
+
+  def create
+    @sled = Sled.new(sled_params)
+    if @sled.valid?
+      @sled.save
+      render json: @sled
+    end
+  end
+
+  def update
+    @sled = Sled.find(params[:id])
+    if @sled.update(sled_params)
+      render json: @sled
+    end
+  end
+
+  def destroy
+    @sled = Sled.find(params[:id])
+    @sled.destroy
+  end
+
+
+  private
+
+  def sled_params
+    params.require(:sled).permit(:name, :imageUrl)
+  end
 end
